4b17cb45940c438133c0e0151f18ad4ff9c79945
amaruli1 <104330748+amaruli1@users.noreply.github.com>
2022-12-09 13:07:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "rllxxi" //namabot kalian
-global.ownername= "Amaruli" //nama kalian
+global.botname = "xxos" //namabot kalian
+global.ownername= "vxaoi" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
@@ -42,8 +42,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = 'rllxxi' //sticker wm ubah
-global.author = 'Di Buat Oleh Amaruli' //sticker wm ganti nama kalian
+global.packname = 'vvsoz' //sticker wm ubah
+global.author = 'Di Buat Oleh xxxsoa' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
